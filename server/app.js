db38b47af68d300ea1e4eae43d32da56f32f7d77
Marjorie <cobero.marjorie@gmail.com>
2021-11-09 10:41:47 -0800

    inital commit

--- server/app.js
+++ server/app.js
@@ -1,10 +1,19 @@
 // import files and packages up here
-
+var sdTopSpotsData = require("./data.json")
 
 // create your express server below
-var app;
+var express = require("express");
+var morgan = require("morgan");
+var app = express();
 
 // add your routes and middleware below
+app.use(morgan("dev"));
+app.get("/", function(req, res){
+    res.status(200).send("West Side");
+});
+app.get("/data", function(req, res) {
+    res.status (200).send(sdTopSpotsData);
+});
 
 // finally export the express application
 module.exports = app;
